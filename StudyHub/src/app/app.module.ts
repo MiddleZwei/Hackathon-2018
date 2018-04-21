8e44f84e26443870e7a9265329d47a76fa2ff5ca
Illia Halych <middlezwei@gmail.com>
2018-04-22 00:37:04 +0300

    events

--- StudyHub/src/app/app.module.ts
+++ StudyHub/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AuthService } from './auth.service';
+import { EventsComponent } from './events/events.component';
 
 
 // import * as firebase from 'firebase';
@@ -22,7 +23,8 @@ import { AuthService } from './auth.service';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    EventsComponent
     
   ],
   imports: [
